cae0f7a0ea9254bbe011d98b4289cdf66b8f0fad
idnm <nnemkov@gmail.com>
2021-10-14 14:38:35 +0300

    Introduce parameter shift rule. Works fine, but waay slower than autodiff. Would be interesting to know why.

--- jax_circuits.py
+++ jax_circuits.py
@@ -1,7 +1,7 @@
 import numpy as np
 import jax.numpy as jnp
 
-from jax import random, value_and_grad, jit, lax
+from jax import random, value_and_grad, jit, lax, custom_jvp
 import optax
 
 from qiskit import QuantumCircuit
@@ -70,6 +70,38 @@ def disc(u, u_target):
     """Discrepancy between two unitary matrices."""
     n = u_target.shape[0]
     return 1 - jnp.abs((u.conj() * u_target).sum()) / n
+
+
+def trace_product(u, v):
+    return (u * v.conj().T).sum()
+
+
+def trace2(u, v, angles):
+    return jnp.abs(trace_product(u(angles), v))**2
+
+
+def all_shifts(u, angles, s):
+    basis_shifts = jnp.identity(len(angles))
+    return vmap(lambda x: u(angles+x))(s*basis_shifts)
+
+
+@partial(custom_jvp, nondiff_argnums=(0, 1, ))
+def shift_trace2(u, v, angles):
+    return trace2(u, v, angles)
+
+
+@shift_trace2.defjvp
+def shift_trace2_jvp(u, v, primals, tangents):
+    angles, = primals
+    tangent_angels, = tangents
+
+    tr = trace_product(u(angles), v)
+    tr_shifted_vector = all_shifts(lambda a: trace_product(u(a), v), angles, jnp.pi)
+
+    ans = jnp.abs(tr)**2
+    der = (tr_shifted_vector * tr.conj()).real * tangent_angels
+
+    return ans, der.sum()
 
 
 @partial(jit, static_argnums=(0, 1,))
