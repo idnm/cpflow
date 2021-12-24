36cc13fb801cfbe9b59803b7f5bc1c9dc55b55a3
idnm <nnemkov@gmail.com>
2021-12-24 21:45:51 +0300

    Implementation and experiments with natural gradient and adam-natural gradient. No clear winner.

--- matrix_utils.py
+++ matrix_utils.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+from jax import grad, jacfwd
 
 
 def theoretical_lower_bound(n):
@@ -34,3 +35,16 @@ def disc2(u, u_target):
     n = u_target.shape[0]
     return 1 - jnp.abs((u * u_target.conj()).sum()) ** 2 / n ** 2
 
+
+def fubini_study(u_func, x, relative_coeff=1):
+    u = u_func(x)
+    u_norm2 = jnp.abs(trace_prod(u, u))
+    u_jac = jacfwd(u_func)(x)
+
+    dudu = jnp.tensordot(u_jac, u_jac.conj(), axes=[[0, 1], [0, 1]])
+    udu = jnp.tensordot(u_jac, u.conj(), axes=[[0, 1], [0, 1]])
+
+    Gij = dudu/u_norm2 - relative_coeff*jnp.outer(udu.conj(), udu)/u_norm2**2
+    gij = jnp.real(Gij)
+
+    return gij
--- optimization.py
+++ optimization.py
@@ -16,9 +16,13 @@ def random_angles(num_angles, key=None):
     return random.uniform(key, (num_angles, ), minval=0, maxval=2*jnp.pi)
 
 
-@partial(jit, static_argnums=(0, 1,))
-def optax_update_step(loss_and_grad, opt, opt_state, params):
+@partial(jit, static_argnums=(0, 1, 4))
+def optax_update_step(loss_and_grad, opt, opt_state, params, preconditioner_func):
+    if preconditioner_func is None:
+        preconditioner_func = lambda x, y: y
+
     loss, grads = loss_and_grad(params)
+    grads = preconditioner_func(params, grads)
     updates, opt_state = opt.update(grads, opt_state)
     params = optax.apply_updates(params, updates)
     return params, opt_state, loss
@@ -27,6 +31,7 @@ def optax_update_step(loss_and_grad, opt, opt_state, params):
 def optax_minimize(cost_func,
                    num_params,
                    opt,
+                   preconditioner_func=None,
                    initial_params=None,
                    num_iterations=5000,
                    target_disc=1e-7):
@@ -41,62 +46,71 @@ def optax_minimize(cost_func,
     params_history = []
     loss_history = []
     for _ in range(num_iterations):
-        params, opt_state, loss = optax_update_step(loss_and_grad, opt, opt_state, params)
+        params, opt_state, loss = optax_update_step(loss_and_grad, opt, opt_state, params, preconditioner_func)
         params_history.append(params)
         loss_history.append(loss)
         if loss < target_disc:
             break
-        params_history.append(params)
-        loss_history.append(loss)
 
     return params_history, loss_history
 
 
 @partial(jit, static_argnums=(0, 1))
-def gradient_descent_update_step(cost_func, preconditioner_func, params):
+def gradient_descent_update_step(cost_func, preconditioner_func, params, learning_rate):
 
     loss_and_grad = value_and_grad(cost_func)
     loss, grads = loss_and_grad(params)
-    new_params = params - preconditioner_func(params, grads)
+    new_params = params - learning_rate*preconditioner_func(params, grads)
     new_loss = cost_func(new_params)
     return new_params, new_loss
 
 
-def plain_hessian_preconditioner(cost_func, tikhonov_delta=1e-4, learning_rate=0.1):
+def plain_hessian_preconditioner(cost_func, tikhonov_delta=1e-4):
 
     def preconditioner(params, grads):
         reg_hess = hessian(cost_func)(params) + tikhonov_delta*jnp.identity(len(params))
-        return learning_rate * jnp.linalg.inv(reg_hess) @ grads
+        return jnp.linalg.inv(reg_hess) @ grads
 
     return preconditioner
 
 
-def sparse_hessian_preconditioner(cost_func, tikhonov_delta=1e-4, learning_rate=0.1):
+def sparse_hessian_preconditioner(cost_func, tikhonov_delta=1e-4):
 
     def hvp(f, primals, tangents):
         return jvp(grad(f), (primals,), (tangents,))[1]
 
     def preconditioner(params, grads):
         sol = jax.scipy.sparse.linalg.cg(lambda x: hvp(cost_func, params, x)+tikhonov_delta*x, grads)[0]
-        return learning_rate * sol
+        return sol
+
+    return preconditioner
+
+
+def plain_natural_preconditioner(u_func, tikhonov_delta=1e-4):
+    def preconditioner(params, grads):
+        g = fubini_study(u_func, params)+tikhonov_delta*jnp.identity(len(grads))
+        return jnp.linalg.inv(g) @ grads
 
     return preconditioner
 
 
 def gradient_descent_minimize(cost_func,
                               num_params,
-                              preconditioner_func,
+                              preconditioner_func=None,
+                              learning_rate=0.1,
                               initial_params=None,
                               num_iterations=5000,
                               target_loss=1e-7):
     if initial_params is None:
         initial_params = random_angles(num_params)
+    if preconditioner_func is None:
+        preconditioner_func = lambda x, y: y
 
     params = initial_params
     params_history = []
     loss_history = []
     for _ in range(num_iterations):
-        params, loss = gradient_descent_update_step(cost_func, preconditioner_func, params)
+        params, loss = gradient_descent_update_step(cost_func, preconditioner_func, params, learning_rate)
         params_history.append(params)
         loss_history.append(loss)
         if loss < target_loss:
