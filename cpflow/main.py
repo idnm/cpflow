7a165b6e9939ee6fa52ef28a4fcb270882aa79c2
idnm <nnemkov@gmail.com>
2022-04-19 22:07:19 +0300

    Docstrings to API classes.

--- cpflow/main.py
+++ cpflow/main.py
@@ -147,7 +147,18 @@ def build_unitary(num_qubits, entangling_gate_name, rotation_gates, placements, 
 
 
 class Ansatz:
+    """Building and training template circuits.
 
+    Attributes:
+        num_qubits (int): number of qubits.
+        entangling_gate_name (str): type of the entagling gate in the ansatz, either 'cx', 'cz' or 'cp'.
+        rotation_gates (str): type of 1q gates in the entagling blocks, e.g. 'xz' or 'xyz', can be an arbitrary string of basis letters.
+        placements (dict): specifies where entangling gates are placed.
+
+    Methods:
+        circuit (angles): qiskit circuit of the ansatz. For unspecified angles returns parametrized circuit.
+        learn (array): run optimization routine to minimize Hilbert-Schmidt distance to the array.
+    """
     def __init__(self, num_qubits, entangling_gate_name, placements, rotation_gates='xyz'):
 
         self.num_qubits = num_qubits
@@ -229,6 +240,23 @@ class Ansatz:
 
 
 class Decomposition:
+    """Storing and processing decompositions.
+
+    Attributes:
+        unitary_loss_func (callable): function of a unitary that was minimized.
+        circuit (qiskit circuit): circuit of the decomposition.
+        unitary (array): unitary matrix of the decomposition.
+        label: name of the decomposition.
+        loss: value of the `unitary_loss_func`.
+        type: 'Approximate` for raw decompositions, sometimes can be impoved to `Rational` or `Clifford+T` by the `refine` method.
+        cz_count: total number of CZ gates.
+        cz_depth: circuit depth with respect to CZ gates.
+        t_count: for Clifford+T `type` total number of T and T dagger gates.
+        t_depth: for Clifford+T `type` circuit depth with respect to T and T dagger gates.
+
+    Methods:
+        refine(): attempts to simplify 1q angles in the circuit, represent them as rational multiples of pi and/or translate to the Clifford+T basis.
+    """
 
     def __init__(self, unitary_loss_func, circuit, label='', type='Approximate'):
 
@@ -252,7 +280,7 @@ class Decomposition:
         self._decomposer = None
 
     @classmethod
-    def from_cp_circuit(cls, unitary_loss_func, u_func, circ_func, angles, label):
+    def _from_cp_circuit(cls, unitary_loss_func, u_func, circ_func, angles, label):
         qc = circ_func(angles)
         qc = cp_to_cz_circuit(qc, cp_threshold=1e-6)
         qc = convert_to_ZXZ(qc)
@@ -309,6 +337,22 @@ class RegularizationOptions:
 
 @dataclass
 class BasicOptions:
+    """Dataclass keeping options needed for both static and adaptive synthesis.
+
+    Attributes:
+        num_samples (int): number of initial conditions in multi-start optimization.
+        method (str): optimization method, currently only 'adam' is well tested.
+        learning_rate (str): learning rate for the optimizer.
+        num_gd_iterations (int): number of optimizer updates at the raw sampling stage.
+        cp_distribution (str): 'uniform' for uniform initialization of CP angles, '0' for zero initialization.
+        entry_loss (float): acceptable loss to deem a CP template as prospective.
+        target_loss (float): threshold value of loss to consider a CZ circuit to be a valid decomposition.
+        threshold_cp (float): project CP gates to identity or CZ if CP angle is within `threshold_cp` radian away.
+        learning_rate_at_verification (float): learning rate to use at the verification of CZ circuits.
+        num_gd_iterations_at_verification (int): number of optimizer updates at the verification of CZ circuits.
+        random_seed (int): seed controlling sampling of the initial angles and hyperparameters (in adaptive routine).
+        rotation_gates (str): type of 1q gates to use in the templates, e.g. 'xz' or 'xyz'.
+    """
     num_samples: int = 100
     method: str = 'adam'
     learning_rate: float = 0.1
@@ -325,6 +369,14 @@ class BasicOptions:
 
 @dataclass
 class StaticOptions(BasicOptions):
+    """ Options for static synthesis.
+
+    Attributes:
+        num_cp_gates (int): total number of CP gates in the template circuit.
+        r (float): regularization weight.
+        accepted_num_cz_gates (int): verify prospective decompositions if their CZ count is below.
+
+    """
     num_cp_gates: int = -1
     r: float = 0.00055
     accepted_num_cz_gates: int = -1
@@ -338,6 +390,17 @@ class StaticOptions(BasicOptions):
 
 @dataclass
 class AdaptiveOptions(BasicOptions):
+    """ Options for adaptive synthesis.
+
+    min_num_cp_gates (int): lower bound on the number of CP gates in templates.
+    max_num_cp_gates (int): upper bound on the number of CP gates in templates.
+    r_mean (flaot): mean value for the regularization weight.
+    r_variance (float): variance for the regularization weight (lognormal distribution).
+    max_evals (int): how many hyperparameter configurations to evaluate.
+    target_num_cz_gates (int): desired number of CZ gates in the final  decomposition.
+    stop_if_target_reached (bool): continue or stop if `target_num_cz_gates` has been reached.
+    keep_logs (bool): whether to keep extensive logs with raw evaluations.
+    """
     min_num_cp_gates: int = -1
     max_num_cp_gates: int = -1
     r_mean: float = 0.00055
@@ -365,6 +428,23 @@ class AdaptiveOptions(BasicOptions):
 
 @dataclass
 class Results:
+    """Store and manipulate results of static and adaptive routines.
+
+    Attributes:
+        loss_function (callable): Function of a unitary matrix that was minimized.
+        layer (list): List specifying connectivity of the qubits.
+        label (str): Reminder about what was decomposed, e.g. '3q Toffoli on linear topology'.
+        trials : hyperopt trials instance (if results include adaptive stage).
+        decompositions (tuple): all decompositions, in order that they were found.
+        save_to (str): Where to save the results. Default is 'results/label'
+
+    Methods:
+        save(): save results to location specified by `save_to`.
+        load(path): load results from the path (staticmethod).
+        best_hyperparameters(): list all pairs [num_cp_gates, r] tried in adaptive decomposition in order of increasing score.
+        plot_trials(): visualize trials on a 2d scatter plot.
+    """
+
     loss_function: callable
     layer: list
     label: str = ''
@@ -423,10 +503,22 @@ class Results:
 
 
 class Synthesize:
-    """Class for efficient automated decomposing of unitary matrices into CZ+1q gates.
+    """Automated synthesis of unitary matrices into CZ+1q gates.
 
+    Attributes:
+        layer: sequence of pairs corresponding to connections between qubits, ex: [[0,1], [1,2], [0,2]].
+        num_qubits: number of qubits in the circuit to synthesize.
+        unitary_loss_function: function of a unitary matrix that is to be minimized.
+        target_unitary: if provided `unitary_loss_function` is set to Hilbert-Schmidt distance to the `target_unitary`.
+        target_state: if provided `unitary_loss_function` is set to 1-overlap squared with the `target_state`.
+        label:  reminder about what is synthesised, e.g. '3q Toffoli on linear topology'.
+        cp_regularization_func: function determining penalty cost for cp-angles.
 
+    Methods:
+        static(options): synthesis with a fixed CP template and regularization weight.
+        adaptive(options): synthesis with CP template length and regularization weight optimized by hyperopt.
     """
+
     def __init__(self, layer, unitary_loss_func=None, target_unitary=None, label=None, cp_regularization_func=None):
 
         self.layer = layer
@@ -447,7 +539,7 @@ class Synthesize:
             self.cp_regularization_func = make_regularization_function(RegularizationOptions)
 
     @staticmethod
-    def generate_initial_angles(key, num_angles, cp_mask, cp_dist='uniform', batch_size=1):
+    def _generate_initial_angles(key, num_angles, cp_mask, cp_dist='uniform', batch_size=1):
         key, *subkeys = random.split(key, num=batch_size + 1)
         initial_angles_array = jnp.array(
             [random_cp_angles(num_angles, cp_mask, cp_dist=cp_dist, key=k)
@@ -456,14 +548,14 @@ class Synthesize:
         return initial_angles_array
 
     @staticmethod
-    def plot_raw(res):
+    def _plot_raw(res):
         plt.plot(res['regloss'], label='regloss')
         plt.plot(res['loss'], label='loss')
         plt.plot(res['reg'], label='reg')
         plt.yscale('log')
         plt.legend()
 
-    def generate_raw(self, options, initial_angles_array=None, keep_history=False):
+    def _generate_raw(self, options, initial_angles_array=None, keep_history=False):
 
         anz = Ansatz(self.num_qubits, 'cp', fill_layers(self.layer, options.num_cp_gates), options.rotation_gates)
         loss_func = lambda angles: self.unitary_loss_func(anz.unitary(angles))
@@ -473,7 +565,7 @@ class Synthesize:
 
         key = random.PRNGKey(options.random_seed)
         if initial_angles_array is None:
-            initial_angles_array = Synthesize.generate_initial_angles(
+            initial_angles_array = Synthesize._generate_initial_angles(
                 key,
                 anz.num_angles,
                 anz.cp_mask,
@@ -494,7 +586,7 @@ class Synthesize:
 
         return raw_results
 
-    def evaluate_raw(self, raw_results, options, disable_tqdm=False):
+    def _evaluate_raw(self, raw_results, options, disable_tqdm=False):
 
         # options = Decompose.updated_options(Decompose.default_static_options, options)
         anz = Ansatz(self.num_qubits, 'cp', fill_layers(self.layer, options.num_cp_gates), options.rotation_gates)
@@ -529,7 +621,7 @@ class Synthesize:
 
     def _make_decomposition(self, u_func, circ_func, best_angs, static_options=None, adaptive_options=None, circuit=None):
         if circuit is None:
-            circuit = Decomposition.from_cp_circuit(
+            circuit = Decomposition._from_cp_circuit(
                 self.unitary_loss_func,
                 u_func,
                 circ_func,
@@ -543,6 +635,16 @@ class Synthesize:
         return d
 
     def static(self, options, save_results=True, save_to=''):
+        """ Unitary synthesis with a fixed CP template and regularization weight.
+
+        Args:
+            options: instance of `StaticOptions`.
+            save_results: whether to save results on disk or not.
+            save_to: if provided overwrites default saving path.
+
+        Returns:
+            results: instance of `Results` class.
+        """
 
         results = Synthesize._initialize_results(self, save_results, save_to)
 
@@ -550,10 +652,10 @@ class Synthesize:
         print('\n', options)
 
         print('\nComputing raw results...')
-        raw_results = Synthesize.generate_raw(self, options)
+        raw_results = Synthesize._generate_raw(self, options)
 
         print('\nSelecting prospective results...')
-        raw = Synthesize.evaluate_raw(self, raw_results, options)
+        raw = Synthesize._evaluate_raw(self, raw_results, options)
         prospective_results = raw
         prospective_results = [res for res in prospective_results if res[0] <= options.accepted_num_cz_gates]
         successful_results = []
@@ -594,6 +696,15 @@ class Synthesize:
                  options,
                  save_results=True,
                  save_to=''):
+        """Unitary synthesis with the length of CP template and regularization weight optimized by hyperopt.
+
+        Args:
+            options: instance of `AdaptiveOptions` class.
+            save_results: whether to save results on disk or not.
+            save_to: if provided overwrites default saving path.
+        Returns:
+            results: instance of `Results` class.
+        """
 
         def objective_from_cz_distribution(random_seed, search_params):
             """Evalueates quality of the hyperparameter configuration.
@@ -607,9 +718,9 @@ class Synthesize:
             static_options = options.get_static(num_cp_gates, r)
             static_options.random_seed = random_seed
 
-            raw_results = Synthesize.generate_raw(self, static_options)
+            raw_results = Synthesize._generate_raw(self, static_options)
 
-            evaluated_results = Synthesize.evaluate_raw(
+            evaluated_results = Synthesize._evaluate_raw(
                 self,
                 raw_results,
                 static_options,
